d14b1ca5558a8285609d08d8da7ea4c2e05dc7e1
Siddhartha Vanam <59271771+siddley1001@users.noreply.github.com>
2021-03-05 15:15:32 -0500

    Update econ_dashboard.py

--- econ_dashboard.py
+++ econ_dashboard.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import requests
-import config
-import psycopg2, psycopg2.extras
-import plotly.graph_objects as go
+# import config
+# import psycopg2, psycopg2.extras
+# import plotly.graph_objects as go
 
 
