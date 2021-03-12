3f7221a5099c5ee7a82c86968a091cc4b1c49cd4
Siddhartha Vanam <vanamsid@gmail.com>
2021-03-12 00:49:49 -0500

    Completed Labor Market Page
    Signed-off-by: Siddhartha Vanam <vanamsid@gmail.com>

--- fed_challenge/econ_dashboard.py
+++ fed_challenge/econ_dashboard.py
@@ -1,5 +1,5 @@
 import os
-
+from datetime import datetime
 import pandas as pd
 import streamlit as st
 from fredapi import Fred
@@ -18,46 +18,61 @@ econ_dictionary = {
     'NETEXC': ['NET Exports $B'], 'EXPGSC1': ['Exports $B'], 'IMPGSC1': ['Imports $B'],
     ##Housing Market
     'NHSUSSPT': ['Total New Houses Sold (1,000s of Units)'],'EXHOSLUSM495S': ['Existing Houses Sold (1,000s of Units)'], 'MNMFS': ['Months on Maarket'], 'USSTHPI': ['House Price Index'],
-    ##Manufacturing Market
+    # Manufacturing Market
     'IPMAN': ['Industrial Production Manufacturing Index'], 'IPG331S': ['Durable Goods - Primary Metals'],
     'IPG334S': ['Durable Goods - Computer and Electronic Products'], 'IPG3361T3S': ['Durable Goods - Motor Vehicles and Parts'],
     'IPG337S': ['Durable Goods - Furniture and related products'], 'IPG315A6S': ['Non-Durable Goods - Apparel and Leather Goods'],
     'GFDEBTN': ['Public Debt $M'], 'GFDEGDQ188S': ['Public Debt/Gross GDP Ratio'], 'MTSDS133FMS': ['Federal Surplus or Deficit'], 'FYFSGDA188S': ['Federal Surplus or Deficit as Ratio of GDP'],
-    'NFCI': ['NFCI']
+    'NFCI': ['NFCI'],
+
     # Labor Market
+    'UNRATE': ['U3 Rate %'], 'U6RATE': ['U6 Rate %'], 'NROU': ['Natural Unemployment Rate %'],
+    'CIVPART': ['Cumm. LFPR %'], 'LNS11300002': ['Women LFPR%'], 'LNS11300001': ['Men LFPR%'],
+    'LNS11300012': ["16-19yrs LFPR %"], 'LNS11300036': ['20-24yrs LFPR %'] ,'LNS11300060': ['25-54yrs LFPR %'], 'LNS11324230': ['55+yrs LFPR %'],
+    'LNS11300003': ['White LFPR %'], 'LNS11300006': ['Black LFPR %'], 'LNS11300009': ['Hispanic LFPR %'], 'LNU01332183': ['Asian LFPR %'],
+    'ICSA': ['Initial Jobless Claims'], 'IC4WSA': ['4 wk MA of Initial Claims'], 'CCSA': ['Continued Claims (Insured Unempl.)'], 'CC4WSA': ['4wk MA of Continued Claims'],
+    'FRBKCLMCIM': ['Labor Market Momentum'], 'FRBKCLMCILA': ['Labor Market Level of Activity']
+
+    #Price Level and Interest Rates
+
 }
 
-
-def to_df(series_name, start_date, end_date):
-    series = fred.get_series(series_name, start_date, end_date)
+# Helper Functions
+def to_df(series_name, start, end):
+    series = fred.get_series(series_name, start, end)
     df = pd.DataFrame(series, columns=econ_dictionary[series_name])
-    df.index = df.index.date
+    # df.index = df.index.date
     return df
+
+def show_chart(df):
+    if len(df) > 1:
+        st.line_chart(df)
+    else:
+        st.warning('\nSelect an earlier START date to view a line chart over time')
 
 major_selection = st.sidebar.selectbox(
     'Explore Indicators for:',
     ('Overall Economic Activity', 'Labor Market', 'Price Level and Interest Rates')
 )
 
-start_date = st.sidebar.date_input('START Date')
+start_date = st.sidebar.date_input('START Date', datetime(1,1,20))
 end_date = st.sidebar.date_input('END Date')
+date_condition = start_date < end_date
 
 if major_selection == 'Overall Economic Activity':
     st.header('Overall Economic Activity')
 
     st.subheader('Gross Domestic Product (GDP)')
     gdp = to_df('GDPC1', start_date, end_date)
-
-    st.line_chart(gdp)
+    show_chart(gdp)
     st.write('Updates *Quarterly*')
 
     st.subheader('GDP/Capita')
     gdp_percap = to_df('A939RC0Q052SBEA', start_date, end_date)
-    st.line_chart(gdp_percap)
+    show_chart(gdp_percap)
     st.write('Updates *Quarterly*')
 
     st.write('Use the drop box to Look at the GDP through its 4 main components')
-
     gdp_components = st.selectbox("4 Main Components",
                                   ('Consumption', 'Investment', 'Government Expenditure', 'Net Exports'))
     if gdp_components == 'Consumption':
@@ -67,7 +82,7 @@ if major_selection == 'Overall Economic Activity':
         c_services = to_df('PCESVC96', start_date, end_date)
         c_total = pd.concat([c, c_goods, c_services], axis=1)
 
-        st.line_chart(c_total)
+        show_chart(c_total)
         st.write('Updates *Quarterly*')
 
     if gdp_components == 'Investment':
@@ -80,7 +95,7 @@ if major_selection == 'Overall Economic Activity':
         i_inventory = to_df('CBIC1', start_date, end_date)
         i_total = pd.concat([i, i_res, i_nonres], axis=1)
 
-        st.line_chart(i_total)
+        show_chart(i_total)
         st.write('Updates *Quarterly*')
 
         inv_change = st.checkbox('Change in Real Private Inventory')
@@ -95,7 +110,7 @@ if major_selection == 'Overall Economic Activity':
         gov_statelocal = to_df('SLCEC1', start_date, end_date)
         gov_total = pd.concat([gov_exandinv, gov_fed, gov_statelocal], axis=1)
 
-        st.line_chart(gov_total)
+        show_chart(gov_total)
         st.write('Updates *Quarterly*')
 
     if gdp_components == 'Net Exports':
@@ -105,35 +120,35 @@ if major_selection == 'Overall Economic Activity':
         imports = to_df('IMPGSC1', start_date, end_date)
         netexports = pd.concat([nex, exports, imports], axis=1)
 
-        st.line_chart(netexports)
+        show_chart(netexports)
         st.write('Updates *Quarterly*')
 
     st.header('Housing Market')
     st.subheader('New Home Sales')
     new_homes = to_df('NHSUSSPT', start_date, end_date)
-    st.line_chart(new_homes)
+    show_chart(new_homes)
     st.write('Updates *Monthly*')
 
     st.subheader('Existing Home Sales')
     exist_homes = to_df('EXHOSLUSM495S', start_date, end_date)
-    st.line_chart(exist_homes)
+    show_chart(exist_homes)
     st.write('Updates *Monthly*')
 
     st.subheader('Median Months on Market for New Homes')
     months_on_market = to_df('MNMFS', start_date, end_date)
-    st.line_chart(months_on_market)
+    show_chart(months_on_market)
     st.write('Updates *Monthly*')
 
 
     st.subheader('Federal Housing Financing Agency Price Index')
     fhfi = to_df('USSTHPI', start_date, end_date)
-    st.line_chart(fhfi)
+    show_chart(fhfi)
     st.write('Updates *Quarterly*')
 
 
     st.header('Manufacturing Sector')
     naics_ipmanu = to_df('IPMAN', start_date, end_date)
-    st.line_chart(naics_ipmanu)
+    show_chart(naics_ipmanu)
     st.write('Updates *Monthly*')
 
 
@@ -158,59 +173,142 @@ if major_selection == 'Overall Economic Activity':
     for int in range(len(manu_checks)):
         if manu_checks[int]:
             manu_sectors_todisp.append(manudf_list[int])
-    if len(manu_sectors_todisp) ==  0:
-        st.write('No Boxes are checked')
+    if len(manu_sectors_todisp) == 0:
+        st.warning('No Boxes are checked')
 
-    if len(manu_sectors_todisp) >  0:
+    if len(manu_sectors_todisp) > 0:
         final_manudf = pd.concat(manu_sectors_todisp, axis=1)
-        st.line_chart(final_manudf)
+        show_chart(final_manudf)
         st.write('Updates *Monthly*')
 
     st.header('US National Balance Sheet')
     st.subheader('Federal Debt: Total Public Debt')
     debt = to_df('GFDEBTN', start_date, end_date)
-    st.line_chart(debt)
+    show_chart(debt)
     st.write('Updates *Quarterly*')
 
     st.subheader('Debt/GDP Ratio')
     debt_to_gdp = to_df('GFDEGDQ188S', start_date, end_date)
-    st.line_chart(debt_to_gdp)
+    show_chart(debt_to_gdp)
     st.write('Updates *Quarterly*')
 
 
     st.subheader('Federal Surplus or Deficit')
     surp_or_def = to_df('MTSDS133FMS', start_date, end_date)
-    st.line_chart(surp_or_def)
+    show_chart(surp_or_def)
     st.write('Updates *Monthly*')
 
 
     st.subheader('Surplus or Deficit/GDP Ratio')
     surp_or_def_ratio = to_df('FYFSGDA188S', start_date, end_date)
-    st.line_chart(surp_or_def_ratio)
+    show_chart(surp_or_def_ratio)
     st.write('Updates *Monthly*')
 
 
     st.header('Credit Market')
     st.subheader('National Financial Conditions Index ')
     nfci = to_df('NFCI', start_date, end_date)
-    st.line_chart(nfci)
+    show_chart(nfci)
     st.write('Updates *Weekly*')
 
 if major_selection == 'Labor Market':
     st.header('Labor Market')
 
     st.subheader('Unemployment Rates (U3 and U6)')
+    u3_rate = to_df('UNRATE',start_date, end_date)
+    u6_rate = to_df('U6RATE', start_date, end_date)
+    unemployment_rates = pd.concat([u3_rate, u6_rate], axis=1)
+    show_chart(unemployment_rates)
+    st.write('Updates Monthly')
+
+    st.subheader('Natural Rate of Unemploymnet (Long-Term)')
+    natural_urate = to_df('NROU', start_date, end_date)
+    show_chart(natural_urate)
+    st.write('Updates Quarterly')
+
 
     st.subheader('Labor Force Participation Rates (LFPR)')
-    #split Participation Rates by Race and Age with checkboxes
-    st.subheader('Initial and Continuing Jobless Claims')
+    lfpr_total = to_df('CIVPART', start_date, end_date)
+    show_chart(lfpr_total)
+    st.write('Updates Quarterly')
+
+    lfpr_select = st.selectbox('View the LFPR by demographic:',
+                               ('Age', 'Gender', 'Race'))
+
+    if lfpr_select == 'Age':
+        lfpr_younger = to_df('LNS11300012', start_date, end_date)
+        lfpr_young = to_df('LNS11300036', start_date, end_date)
+        lfpr_middle = to_df('LNS11300060', start_date, end_date)
+        lfpr_old = to_df('LNS11324230', start_date, end_date)
+        lfpr_age_df = pd.concat([lfpr_younger, lfpr_young,
+                                 lfpr_middle, lfpr_old], axis=1)
+        show_chart(lfpr_age_df)
+
+    if lfpr_select == 'Gender':
+        lfpr_female = to_df('LNS11300002', start_date, end_date)
+        lfpr_male = to_df('LNS11300001', start_date, end_date)
+        lfpr_gender_df = pd.concat([lfpr_female, lfpr_male], axis=1)
+        show_chart(lfpr_gender_df)
+
+    if lfpr_select == 'Race':
+        white_lfpr = st.checkbox('White')
+        black_lfpr = st.checkbox('Black')
+        hispanic_lpr = st.checkbox('Hispanic')
+        asian_lfpr = st.checkbox('Asian')
+
+        white_lfpr_df = to_df('LNS11300003', start_date, end_date)
+        black_lfpr_df = to_df('LNS11300006', start_date, end_date)
+        hispanic_lpr_df = to_df('LNS11300009', start_date, end_date)
+        asian_lfpr_df = to_df('LNU01332183', start_date, end_date)
+
+        lfpr_checks = [white_lfpr, black_lfpr, hispanic_lpr, asian_lfpr]
+        lfpr_df_list = [white_lfpr_df, black_lfpr_df, hispanic_lpr_df, asian_lfpr_df]
+        lfpr_races_todisp = []
+
+        col1, col2 = st.beta_columns([2,2])
+
+        # checks checkboxes
+        for int in range(len(lfpr_df_list)):
+            if lfpr_checks[int]:
+                lfpr_races_todisp.append(lfpr_df_list[int])
+        if len(lfpr_races_todisp) == 0:
+            st.warning('No Boxes are checked')
+
+        if len(lfpr_races_todisp) > 0:
+            final_lfpr_races = pd.concat(lfpr_races_todisp, axis=1)
+            with col1:
+                # st.header of Combinations of races
+                #Cut the |T00:... from the Datetime Index
+                show_chart(final_lfpr_races)
+                st.write('Updates *Monthly*')
+            with col2:
+                st.write(final_lfpr_races)
+
+    st.subheader('Initial Jobless Claims')
+    init_claims = to_df('ICSA', start_date, end_date)
+    init_ma_claims = to_df('IC4WSA', start_date, end_date)
+    init_total = pd.concat([init_claims, init_ma_claims], axis=1)
+    show_chart(init_total)
+    st.write('Updates Weekly')
+
+    st.subheader('Continuing Jobless Claims')
+    cont_claims = to_df('CCSA', start_date, end_date)
+    cont_ma_claims = to_df('CC4WSA', start_date, end_date)
+    cont_total = pd.concat([cont_claims, cont_ma_claims], axis=1)
+    show_chart(cont_total)
+    st.write('Updates Weekly')
 
     st.subheader('KC Fed Labor Market Conditions: Momentum and Overall Activity')
+    kc_momentum = to_df('FRBKCLMCIM', start_date, end_date)
+    kc_activity = to_df('FRBKCLMCILA', start_date, end_date)
+    kc = pd.concat([kc_momentum, kc_activity], axis=1)
+    show_chart(kc)
+    st.write('Updates Monthly')
 
 
 if major_selection == 'Price Level and Interest Rates':
     st.header('Price Level and Interest Rates')
-    st.write("These indicators primarily include the Fed's Tools and Inflation")
+    st.write("Includes the Fed's Tools and Inflation indicators")
     st.subheader("Federal Reserve's Interest Rates")
     st.subheader("Federal Reserve's Policy Tools")
     st.subheader("Yield Curves & Federal Reserve's Balance Sheet")
